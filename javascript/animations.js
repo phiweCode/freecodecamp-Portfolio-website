caf1065c6862460ad3aeffca844597fb4c126617
unknown <thabo.s.mngoma@gmail.com>
2022-03-07 15:45:15 +0200

    Updates

---
+++ javascript/animations.js
@@ -0,0 +1,6 @@
+anime({
+  targets: ".socials-icons",
+  translateX: 45,
+  delay: anime.stagger(400, { easing: "easeOutQuad" }), 
+
+});
---
+++ javascript/app.js
@@ -0,0 +1,57 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicGhpd2UxOTk4IiwiYSI6ImNrenIxZ28waDBmbmQyb211dTJjd252cHEifQ.vmtqKZk98SBUtb_gP5ZQuQ";
+
+
+
+navigator.geolocation.getCurrentPosition(successLocation,errorLocation, {enableHighAccuracy: true})
+
+function successLocation(position) 
+{ 
+     console.log(position) 
+     setUpMap([position.coords.longitude, position.coords.latitude]); 
+} 
+
+function errorLocation(){
+    setUpMap[(-28.0473, 26.2041)];
+}; 
+
+function setUpMap(center)
+{
+  var map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/phiwe1998/ckzr1ox7z00oo14l9xbgote3l",
+    center: center,
+    zoom: 6,
+  });
+
+  // add zoom and rotation controls to the map
+
+  map.addControl(new mapboxgl.NavigationControl()); 
+
+  
+
+}  
+
+//menu -btn   
+
+const menuBtn = document.querySelector(".menu-container");
+let menuCheck = true;
+const hamburgerMenu = document.querySelector(".menu-container"); 
+const menuList = document.querySelector(".page-links");  
+
+menuBtn.addEventListener("click", () => { 
+  
+  menuList.classList.toggle("active-links"); 
+  
+  if (!menuCheck) {
+    menuBtn.classList.add("open"); 
+    menuList.classList.add("active-links"); 
+    menuCheck = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuList.classList.remove("active-links"); 
+    menuCheck = false;
+  }
+});
+
+
